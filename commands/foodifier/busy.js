9d1ecace7cf922b755f1f4e460cb70e26c8704c4
Sh4d0wb0Y <jaytripathi100@gmail.com>
2023-04-14 18:08:29 -0700

    add dh choices with corresponding waitz api names

--- commands/foodifier/busy.js
+++ commands/foodifier/busy.js
@@ -2,6 +2,10 @@ const { SlashCommandBuilder, EmbedBuilder, Embed } = require('discord.js');
 const { WAITZ_URL, BASE_URL, LOCATIONS, MEAL_URL, MEALS, DIVIDERS, EMOJIS } = require('../../urls.js');
 const { fetchMenu, getColor } = require('../../fetch-menu.js');
 
+async function getBusyness(location) {
+
+}
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('busy')
@@ -9,8 +13,15 @@ module.exports = {
         .addStringOption(option =>
             option.setName('location')
             .setDescription('The dining hall or cafe location to get the busyness of')
-            .setRequired(true)),
-    
+            .setRequired(true)
+            .addChoices(
+                { name: 'Cowell/Stevenson', value: 'Cowell / Stevenson Dining Hall' },  
+                { name: 'Crown/Merrill', value: 'Crown / Merrill Dining Hall' },  
+                { name: 'Nine/Ten', value: 'College 9 / John R Lewis Dining Hall' },  
+                { name: 'Porter/Kresge', value: 'Porter / Kresge Dining Hall' }, 
+                { name: 'Oakes Cafe', value: 'Oakes Cafe Main'}
+            )),
+
     async execute(interaction) {
         await interaction.reply('hi');
         console.log(`User ${interaction.user.tag} used command ${interaction}`);
